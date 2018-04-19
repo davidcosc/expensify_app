2d150547a32f8dc2d35fdc2158586d9156845120
david <d.cornel@tu-braunschweig.de>
2018-04-20 00:34:32 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(<p>boilerplate</p>, appRoot);
